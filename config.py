fb2cc8bbbd9a7ba7e14fc4f4c3520e06c1707a46
amengede <amengede27@outlook.com>
2020-09-23 14:32:49 +1000

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+pg.init()
+
+SCREEN_SIZE = (640,480)
+
+#colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+CLOCK = pg.time.Clock()
+FRAMERATE = 60
---
+++ main.py
@@ -0,0 +1,3 @@
+from config import *
+
+pg.display.set_caption("Shooting Game")
